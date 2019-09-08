19b4a57f2ee2417704846ef28d44bd33716e5a20
Gerd Zellweger <mail@gerdzellweger.com>
2019-09-08 13:05:33 -0700

    Make time calculation in klogger more robust and work in VMs

--- src/lib.rs
+++ src/lib.rs
@@ -28,84 +28,75 @@ mod arch;
 use log::{Level, Metadata, Record, SetLoggerError};
 use termcodes::color;
 
-static mut LOGGER: KLogger = KLogger {
-    has_tsc: false,
-    has_invariant_tsc: false,
+/// One Mhz is that many Hz.
+const MHZ_TO_HZ: u64 = 1000 * 1000;
 
-    tsc_start: 0,
-    tsc_frequency: 2_000_000_000,
-};
+/// One Khz is that many Hz.
+const KHZ_TO_HZ: u64 = 1000;
 
-pub struct Writer;
+/// One sec has that many ns.
+const NS_PER_SEC: u64 = 1_000_000_000u64;
 
-impl Writer {
-    /// Obtain a logger for the specified module.
-    pub fn get_module(module: &str) -> Writer {
-        use core::fmt::Write;
-        let mut ret = Writer;
-        write!(&mut ret, "[{}] ", module).expect("Writer");
-        ret
-    }
-
-    pub fn get() -> Writer {
-        Writer
-    }
-}
-
-impl ops::Drop for Writer {
-    /// Release the logger.
-    fn drop(&mut self) {
-        use core::fmt::Write;
-        write!(self, "\r\n").expect("Newline");
-    }
-}
-
-impl fmt::Write for Writer {
-    /// Write stuff to serial out.
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        unsafe {
-            arch::puts(s);
-        }
-        Ok(())
-    }
-}
-
-pub struct WriterNoDrop;
-
-impl WriterNoDrop {
-    pub fn get() -> WriterNoDrop {
-        WriterNoDrop
-    }
-}
-
-impl fmt::Write for WriterNoDrop {
-    /// Write stuff to serial out.
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        unsafe {
-            arch::puts(s);
-        }
-        Ok(())
-    }
-}
+/// Global lock to protect serial line from concurrent printing.
+static mut SERIAL_LINE_MUTEX: spin::Mutex<bool> = spin::Mutex::new(false);
 
 #[derive(Debug)]
 struct KLogger {
+    /// Do we even have a TSC?
+    ///
+    /// If not bad.
     has_tsc: bool,
+    /// Is the underlying TSC invariant?
+    ///
+    /// If not, bad.
     has_invariant_tsc: bool,
+    /// Point in time when this Klogger got initialized
     tsc_start: u64,
     /// Frequency in Hz
-    tsc_frequency: u64,
+    ///
+    /// Sometimes we can't figure this out (yet)
+    tsc_frequency: Option<u64>,
+}
+
+enum ElapsedTime {
+    Undetermined,
+    Nanoseconds(u64),
+    Cycles(u64),
+}
+
+impl fmt::Display for ElapsedTime {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ElapsedTime::Nanoseconds(ns) => write!(f, "{:>10}", ns),
+            // the cyc is added here so we are made aware it's not nano-seconds
+            ElapsedTime::Cycles(cycles) => write!(f, "{:>10} cyc", cycles),
+            ElapsedTime::Undetermined => write!(f, ""),
+        }
+    }
 }
 
 impl KLogger {
     /// Time in nano seconds since KLogger init.
-    fn elapsed_ns(&self) -> u64 {
+    fn elapsed(&self) -> ElapsedTime {
         if self.has_tsc {
             let cur = unsafe { x86::time::rdtsc() };
-            let elapsed = (cur - self.tsc_start) as f64;
-            (elapsed / (self.tsc_frequency as f64 / 1_000_000_000.0)) as u64
+
+            if self.has_invariant_tsc && self.tsc_frequency.is_some() {
+                let elapsed_cycles = cur - self.tsc_start;
+                let tsc_frequency_hz = self.tsc_frequency.unwrap_or(1); // This won't fail, checked by if above
+
+                // Basic is: let ns = elapsed_cycles / (tsc_frequency / NS_PER_SEC);
+                // But we avoid removing all precision with division:
+                let ns = (elapsed_cycles * NS_PER_SEC) / tsc_frequency_hz;
+
+                ElapsedTime::Nanoseconds(ns)
+            } else {
+                // We can't convert cycles to a time
+                ElapsedTime::Cycles(cur)
+            }
         } else {
-            0
+            // We don't know
+            ElapsedTime::Undetermined
         }
     }
 }
@@ -126,9 +117,9 @@ impl log::Log for KLogger {
             };
 
             sprintln!(
-                "{}{:>10}{} [{}{:5}{}] - {}: {}{}{}",
+                "{}{}{} [{}{:5}{}] - {}: {}{}{}",
                 color::Fg(color::LightYellow),
-                self.elapsed_ns(),
+                self.elapsed(),
                 color::Fg(color::Reset),
                 color::Fg(color),
                 record.level(),
@@ -144,38 +135,125 @@ impl log::Log for KLogger {
     fn flush(&self) {}
 }
 
+static mut LOGGER: KLogger = KLogger {
+    has_tsc: false,
+    has_invariant_tsc: false,
+    tsc_start: 0,
+    tsc_frequency: None,
+};
+
+/// A writer for the serial line. It holds a lock so
+/// multiple cores/threads can print at the same time.
+pub struct Writer<'a> {
+    /// Lock on the serial line, it is implicitly released on a drop.
+    #[allow(dead_code)]
+    line_lock: spin::MutexGuard<'a, bool>,
+}
+
+impl<'a> Writer<'a> {
+    /// Obtain a logger for the specified module.
+    pub fn get_module(module: &str) -> Writer<'a> {
+        use core::fmt::Write;
+        let line_lock = unsafe { SERIAL_LINE_MUTEX.lock() };
+        let mut ret = Writer { line_lock };
+        write!(&mut ret, "[{}] ", module).expect("Writer");
+        ret
+    }
+
+    /// Obtain a logger.
+    pub fn get() -> Writer<'a> {
+        let line_lock = unsafe { SERIAL_LINE_MUTEX.lock() };
+        Writer { line_lock }
+    }
+}
+
+impl<'a> ops::Drop for Writer<'a> {
+    /// Release the logger (and the line_lock), end output with a newline.
+    ///
+    /// Serial standard mandates the use of '\r\n' for a newline and
+    /// resetting the curser to the beginning.
+    fn drop(&mut self) {
+        use core::fmt::Write;
+        #[allow(clippy::write_with_newline)]
+        write!(self, "\r\n").expect("Newline");
+    }
+}
+
+impl<'a> fmt::Write for Writer<'a> {
+    /// Write stuff to serial out.
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        unsafe {
+            arch::puts(s);
+        }
+        Ok(())
+    }
+}
+
+/// A writer that doesn't respect the locking procedure and tries to write at all costs.
+///
+/// It's used by sprint at the moment. It can also be useful as part of panics handlers
+/// where we really want to print in all circumstances.
+pub struct WriterNoDrop;
+
+impl WriterNoDrop {
+    pub fn get() -> WriterNoDrop {
+        WriterNoDrop
+    }
+}
+
+impl fmt::Write for WriterNoDrop {
+    /// Write stuff to serial out.
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        unsafe {
+            arch::puts(s);
+        }
+        Ok(())
+    }
+}
+
 pub fn init(level: Level) -> Result<(), SetLoggerError> {
     let cpuid = x86::cpuid::CpuId::new();
 
     unsafe {
-        (&mut LOGGER).has_tsc = cpuid
+        LOGGER.has_tsc = cpuid
             .get_feature_info()
             .map_or(false, |finfo| finfo.has_tsc());
-        (&mut LOGGER).has_invariant_tsc = cpuid
+        LOGGER.has_invariant_tsc = cpuid
             .get_extended_function_info()
             .map_or(false, |efinfo| efinfo.has_invariant_tsc());
+
         if LOGGER.has_tsc {
-            (&mut LOGGER).tsc_start = x86::time::rdtsc();
+            LOGGER.tsc_start = x86::time::rdtsc();
         }
 
-        if cpuid.get_tsc_info().is_some() {
-            // Nominal TSC frequency = ( CPUID.15H.ECX[31:0] * CPUID.15H.EBX[31:0] ) ÷ CPUID.15H.EAX[31:0]
-            (&mut LOGGER).tsc_frequency = cpuid
-                .get_tsc_info()
-                .map_or(2_000_000_000, |tinfo| tinfo.tsc_frequency());
-        } else if cpuid.get_processor_frequency_info().is_some() {
-            (&mut LOGGER).tsc_frequency = cpuid
-                .get_processor_frequency_info()
-                .map_or(2_000_000_000, |pinfo| {
-                    pinfo.processor_max_frequency() as u64 * 1000000
-                });
-        } else if cpuid.get_hypervisor_info().is_some() {
-            let hv = cpuid.get_hypervisor_info().unwrap();
-            (&mut LOGGER).tsc_frequency = hv
-                .tsc_frequency()
-                .map_or(2_000_000_000, |tsc_khz| tsc_khz as u64 * 1000);
-        } else {
-            (&mut LOGGER).tsc_frequency = 2_000_000_000;
+        let tsc_frequency_hz: Option<u64> = cpuid.get_tsc_info().and_then(|tinfo| {
+            if tinfo.nominal_frequency() != 0 {
+                // If we have a crystal clock we can calculate the tsc frequency directly
+                Some(tinfo.tsc_frequency())
+            } else if tinfo.numerator() != 0 && tinfo.denominator() != 0 {
+                // Skylake and Kabylake don't report the crystal clock frequency,
+                // so we approximate with CPU base frequency:
+                cpuid.get_processor_frequency_info().map(|pinfo| {
+                    let cpu_base_freq_hz = pinfo.processor_base_frequency() as u64 * MHZ_TO_HZ;
+                    let crystal_hz =
+                        cpu_base_freq_hz * tinfo.denominator() as u64 / tinfo.numerator() as u64;
+                    crystal_hz * tinfo.numerator() as u64 / tinfo.denominator() as u64
+                })
+            } else {
+                // We couldn't figure out the TSC frequency
+                None
+            }
+        });
+
+        // Check if we run in a VM and the hypervisor can give us the TSC frequency
+        let vmm_tsc_frequency_hz: Option<u64> = cpuid
+            .get_hypervisor_info()
+            .and_then(|hv| hv.tsc_frequency().map(|tsc_khz| tsc_khz as u64 * KHZ_TO_HZ));
+
+        if tsc_frequency_hz.is_some() {
+            LOGGER.tsc_frequency = tsc_frequency_hz;
+        } else if vmm_tsc_frequency_hz.is_some() {
+            LOGGER.tsc_frequency = vmm_tsc_frequency_hz;
         }
 
         // Another way that segfaults in KVM:
